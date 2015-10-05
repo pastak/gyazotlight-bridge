4b12b8df85fb43f9b562940eb710e86ea7aa2aed
pastak <pasta0915@gmail.com>
2015-10-05 14:30:16 +0900

    change hotkey Ctrl+Shift+U

--- common/content.js
+++ common/content.js
@@ -1,7 +1,7 @@
 var parseResponse = require('./parseResponse')
 document.addEventListener('keydown', function(event){
-  // Ctrl + Shift + I
-  if (!(event.keyCode === 73 && event.ctrlKey && event.shiftKey)) return
+  // Ctrl + Shift + U
+  if (!(event.keyCode === 85 && event.ctrlKey && event.shiftKey)) return
   if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.sendMessage) {
     chrome.runtime.sendMessage(null, {name: 'call'}, function(text) {
       parseResponse(text)
